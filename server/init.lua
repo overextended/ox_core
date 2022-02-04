11102a9a84ba5e742c08bf3dfe017af57b42c336
Linden <65407488+thelindat@users.noreply.github.com>
2022-02-04 21:23:00 +1100

    feat(server/init): convar overrides for inventory and npwd

--- server/init.lua
+++ server/init.lua
@@ -1,1 +1,13 @@
 server.PRIMARY_INDENTIFIER = 'ip'
+
+-- Disable population in routing bucket id 60
+-- This routing bucket will be used during character selection
+SetRoutingBucketPopulationEnabled(60, false)
+
+SetConvarReplicated('inventory:framework', 'ox')
+SetConvarReplicated('inventory:trimplate ', 'false')
+SetConvar('npwd:useResourceIntegration', 'true')
+SetConvar('npwd:database', json.encode({
+	playerTable = 'characters',
+	identifierColumn = 'charid',
+}))
