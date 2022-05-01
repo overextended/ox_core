bbf1ff6b6d17b3a97e81252c2315ca52690b971a
Linden <65407488+thelindat@users.noreply.github.com>
2022-05-01 03:59:02 +0000

    refactor(server/functions): export GetIdentifiers

--- server/functions.lua
+++ server/functions.lua
@@ -1,18 +1,10 @@
-local functions = {}
-
-function functions.getIdentifiers(source)
+function Ox.GetIdentifiers(source)
 	local identifiers = {}
 
-	for i = 0, GetNumPlayerIdentifiers(source)-1 do
+	for i = 0, GetNumPlayerIdentifiers(source) - 1 do
 		local prefix, identifier = string.strsplit(':', GetPlayerIdentifier(source, i))
 		identifiers[prefix] = identifier
 	end
 
 	return identifiers
 end
-
-function functions.firstToUpper(str)
-	return str:gsub("^%l", string.upper)
-end
-
-server.functions = functions
