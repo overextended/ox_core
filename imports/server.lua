ea9559059ee0fc1d958cb54bb3077e2756ee2fb6
Linden <65407488+thelindat@users.noreply.github.com>
2022-06-01 16:53:32 +1000

    refactor(imports/server): update hasGroup check
    IsPlayerAceAllowed is great, when you don't already have groups data to parse.

--- imports/server.lua
+++ imports/server.lua
@@ -25,11 +25,37 @@ function lib.getPlayer(player)
 end
 
 function CPlayer:hasGroup(filter)
-	-- todo: support string, array, and hash (need to rewrite group stuff first)
-	for group, minimumGrade in pairs(filter) do
-		if IsPlayerAceAllowed(self.source, ('group.%s:%s'):format(group, minimumGrade == 0 and 1 or minimumGrade)) then
-			return true
+	local type = type(filter)
+
+	if type == 'string' then
+		local grade = self.groups[filter]
+
+		if grade then
+			return filter, grade
 		end
+	elseif type == 'table' then
+		local tabletype = table.type(filter)
+
+		if tabletype == 'hash' then
+			for name, grade in pairs(filter) do
+				local playerGrade = self.groups[name]
+
+				if playerGrade and grade <= playerGrade then
+					return name, playerGrade
+				end
+			end
+		elseif tabletype == 'array' then
+			for i = 1, #filter do
+				local name = filter[i]
+				local grade = self.groups[name]
+
+				if grade then
+					return name, grade
+				end
+			end
+		end
+	else
+		error(("received '%s' when checking player group"):format(filter))
 	end
 end
 
