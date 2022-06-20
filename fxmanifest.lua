33f522bb3b69679a2a3608241a990cc2dcb503ee
Linden <65407488+thelindat@users.noreply.github.com>
2022-06-21 05:08:16 +1000

    chore: version bump
    Arbitrary semver ftw

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ game         'gta5'
 
 --[[ Resource Information ]]--
 name         'ox_core'
-version      '0.0.2'
+version      '0.1.0'
 description  'What have I done?'
 license      'GPL-3.0-or-later'
 author       'overextended'
