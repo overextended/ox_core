896ddb68751a536798ab171f798753762515bda3
Linden <65407488+thelindat@users.noreply.github.com>
2022-08-14 01:06:43 +1000

    refactor: remove vehicle parsing
    Native got removed, so that's cool.

--- server/debug.lua
+++ server/debug.lua
@@ -1,17 +1,1 @@
 if not Shared.DEBUG then return end
-
-lib.addCommand('group.admin', 'parsevehicles', function(source, args)
-    local vehicleData = lib.callback.await('ox:generateVehicleData', source, args.processAll)
-
-    if not args.processAll then
-        local oldData = json.load('files/vehicles.json')
-
-        if oldData then
-            for k, v in pairs(oldData) do
-                vehicleData[k] = v
-            end
-        end
-    end
-
-    SaveResourceFile('ox_core', 'files/vehicles.json', json.encode(vehicleData, { indent = true, sort_keys = true }), -1)
-end, { 'processAll' })
