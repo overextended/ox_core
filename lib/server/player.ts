dd8b2ad6dced891fdbf0f1d4f9cf87da6919a7ed
Linden <65407488+thelindat@users.noreply.github.com>
2022-09-03 09:42:11 +1000

    refactor(lib/server): update class fields and methods
    Some fields were missing, but dead, groups, and coords are no longer used.
    Updates set with the replicated arg, and adds new bindings for 'hasGroup'
    and 'logout'.

--- lib/server/player.ts
+++ lib/server/player.ts
@@ -8,9 +8,9 @@ export class CPlayer {
   charid: number;
   ped: number;
   name: string;
-  dead: boolean;
-  groups: Record<string, number>;
-  coords?: number[];
+  username: string;
+  firstname: string;
+  lastname: string;
 
   constructor(data: any) {
     this.source = data.source;
@@ -18,17 +18,17 @@ export class CPlayer {
     this.charid = data.charid;
     this.ped = data.ped;
     this.name = data.name;
-    this.dead = data.dead;
-    this.groups = data.groups;
+    this.username = data.username;
+    this.firstname = data.firstname;
+    this.lastname = data.lastname;
   }
 
-  getCoords(update?: boolean) {
-    if (update || !this.coords) this.coords = GetEntityCoords(this.ped);
-    return this.coords;
+  getCoords() {
+    return GetEntityCoords(this.ped);
   }
 
-  set(key: string, value: any) {
-    exp(this.source, "set", key, value);
+  set(key: string, value: any, replicated: boolean) {
+    exp(this.source, "set", key, value, replicated);
   }
 
   get(key?: string): any {
@@ -44,28 +44,7 @@ export class CPlayer {
   }
 
   hasGroup(filter: string | string[] | Record<string, number>): [string, number] | undefined {
-    if (typeof filter === "string") {
-      const grade = this.groups[filter];
-
-      if (grade) return [filter, grade];
-    } else if (typeof filter === "object") {
-      if (Array.isArray(filter)) {
-        for (let i = 0; filter.length; i++) {
-          const name = filter[i];
-          const playerGrade = this.groups[name];
-
-          if (playerGrade) return [name, playerGrade];
-        }
-      } else {
-        for (const [name, grade] of Object.entries(filter)) {
-          const playerGrade = this.groups[name];
-
-          if (playerGrade && (grade as number) <= playerGrade) {
-            return [name, playerGrade];
-          }
-        }
-      }
-    }
+    return exp(this.source, "hasGroup", filter);
   }
 
   isPlayerInScope(target: number): boolean {
@@ -74,6 +53,10 @@ export class CPlayer {
 
   triggerScopedEvent(eventName: string, ...args: any) {
     exp(this.source, "triggerScopedEvent", eventName, ...args);
+  }
+
+  logout() {
+    exp(this.source, "logout");
   }
 
   getAccounts() {
