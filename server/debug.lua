b4c3141aed56f7880803f0f8d07065c0244bfa7d
Linden <65407488+thelindat@users.noreply.github.com>
2022-09-12 22:54:10 +1000

    refactor(server/debug): check vehicleData before saving

--- server/debug.lua
+++ server/debug.lua
@@ -3,15 +3,17 @@ if not Shared.DEBUG then return end
 lib.addCommand('group.admin', 'parsevehicles', function(source, args)
     local vehicleData = lib.callback.await('ox:generateVehicleData', source, args.processAll)
 
-    if not args.processAll then
-        local oldData = json.load('files/vehicles.json')
+    if vehicleData and next(vehicleData) then
+        if not args.processAll then
+            local oldData = json.load('files/vehicles.json')
 
-        if oldData then
-            for k, v in pairs(oldData) do
-                vehicleData[k] = v
+            if oldData then
+                for k, v in pairs(oldData) do
+                    vehicleData[k] = v
+                end
             end
         end
-    end
 
-    SaveResourceFile('ox_core', 'shared/files/vehicles.json', json.encode(vehicleData, { indent = true, sort_keys = true }), -1)
+        SaveResourceFile('ox_core', 'shared/files/vehicles.json', json.encode(vehicleData, { indent = true, sort_keys = true }), -1)
+    end
 end, { 'processAll' })
