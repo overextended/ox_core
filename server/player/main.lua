17346af9b1d6a78f8263fe0e04b5f5f9906c7483
SamShanks1 <68856260+SamShanks1@users.noreply.github.com>
2022-09-16 12:46:15 +0100

    feat: set ped model from gender

--- server/player/main.lua
+++ server/player/main.lua
@@ -178,6 +178,7 @@ function Player.loaded(player, character)
     player.charid = character.charid
     player.firstname = character.firstname
     player.lastname = character.lastname
+    player.gender = result.gender
     player.set('groups', {})
 
     result = db.selectCharacterGroups(player.charid)
