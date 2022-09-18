6c06bd5af40eb2f198c41e3f28d8a2478434290b
Linden <65407488+thelindat@users.noreply.github.com>
2022-09-18 17:26:35 +1000

    refactor(imports/server): update exports reference

--- imports/server/vehicle.lua
+++ imports/server/vehicle.lua
@@ -1,12 +1,14 @@
-local ox_core = exports.ox_core
 local CVehicle = {}
-local VehicleExports = {}
-setmetatable(VehicleExports, {
+
+local vehicleExports = {}
+setmetatable(vehicleExports, {
     __index = function(_, index)
-        VehicleExports = Ox.VehicleExports()
-        return VehicleExports[index]
+        vehicleExports = Ox.GetPlayerExports()
+        return vehicleExports[index]
     end
 })
+
+local exp = exports.ox_core
 
 function CVehicle:__index(index)
     local method = CVehicle[index]
@@ -17,11 +19,11 @@ function CVehicle:__index(index)
         end
     end
 
-    local export = VehicleExports[index]
+    local export = vehicleExports[index]
 
     if export then
         return function(...)
-            return ox_core:CVehicle(self.entity, index, ...)
+            return exp:CVehicle(self.entity, index, ...)
         end
     end
 end
@@ -35,22 +37,22 @@ function CVehicle:getCoords(update)
 end
 
 function Ox.GetVehicle(vehicle)
-    vehicle = type(vehicle) == 'table' and vehicle.entity or ox_core:GetVehicle(vehicle)
+    vehicle = type(vehicle) == 'table' and vehicle.entity or exp:GetVehicle(vehicle)
     return vehicle and setmetatable(vehicle, CVehicle)
 end
 
 function Ox.GetVehicleFromNetId(vehicle)
-    vehicle = ox_core:GetVehicleFromNetId(vehicle)
+    vehicle = exp:GetVehicleFromNetId(vehicle)
     return vehicle and setmetatable(vehicle, CVehicle)
 end
 
 function Ox.CreateVehicle(data, coords, heading)
-    local vehicle = ox_core:CreateVehicle(data, coords, heading)
+    local vehicle = exp:CreateVehicle(data, coords, heading)
     return type(vehicle) == 'table' and setmetatable(vehicle, CVehicle) or vehicle
 end
 
 function Ox.GetVehicles(usemetatable)
-    local vehicles = ox_core:GetVehicles()
+    local vehicles = exp:GetVehicles()
 
     if usemetatable then
         for i = 1, #vehicles do
