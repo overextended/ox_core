46c3aa0642df5ac9d67f63cbc2a98f3448155df5
Linden <65407488+thelindat@users.noreply.github.com>
2022-11-22 19:56:30 +1100

    fix(server/class): set correct __index
    Somehow this change wasn't committed.

--- server/class.lua
+++ server/class.lua
@@ -5,22 +5,12 @@ local type = type
 ---@param prototype T
 ---@return { new: fun(obj): T}
 function Class.new(prototype)
-    local class = {}
+    local class = {
+        __index = prototype
+    }
 
     function class.new(obj)
         return setmetatable(obj, class)
-    end
-
-    function class:__index(index)
-        local value = prototype[index]
-
-        if type(value) == 'function' then
-            return function(...)
-                return value(self, ...)
-            end
-        end
-
-        return value
     end
 
     return class
