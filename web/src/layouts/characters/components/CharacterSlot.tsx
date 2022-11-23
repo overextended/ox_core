e2cb84c63eb8c04877f17766e0885c348e6e60fa
Luke <sabolukas03@gmail.com>
2022-11-23 16:25:06 +0100

    style(web): adjust character box styling

--- web/src/layouts/characters/components/CharacterSlot.tsx
+++ web/src/layouts/characters/components/CharacterSlot.tsx
@@ -20,42 +20,42 @@ const Character: React.FC<{ character: CharacterProps, index: number }> = (props
   };
 
   return (
-    <div className='flex'>
-      <div
-        className={`hover-transition flex h-24 w-[85%] flex-col justify-between p-1.5 text-white hover:bg-black/40 ${characterIndex === props.index && 'bg-black/40'}`}
-        onClick={() => setCharacterIndex(props.index)}>
-        <div className='flex  items-center justify-between'>
-          <p className='text-xl'>{`${props.character.firstname} ${props.character.lastname}`}</p>
+    <>
+      <div className='flex'>
+        <div
+          className={` hover-transition flex h-24 w-[85%] flex-col justify-evenly p-1.5 text-white hover:bg-black/40 ${characterIndex === props.index && 'bg-black/40'}`}
+          onClick={() => setCharacterIndex(props.index)}>
+          <p className='truncate text-2xl'>{`${props.character.firstname} ${props.character.lastname}`}</p>
+          <div>
+            <p className='truncate text-sm'>Location: {props.character.location}</p>
+            <p className='truncate text-sm'>Last played: {props.character.last_played}</p>
+          </div>
         </div>
-        <div>
-          <p>Location: {props.character.location}</p>
-          <p>Last played: {props.character.last_played}</p>
-        </div>
+        <Transition
+          show={characterIndex === props.index}
+          as={React.Fragment}
+          enter='scale opacity duration-300'
+          enterFrom='scale-[0.9] opacity-0'
+          enterTo='scale-1 opacity-100'
+          leave='scale duration-300'
+          leaveFrom='scale-1 opacity-100'
+          leaveTo='scale-[0.9] opacity-0'
+        >
+          <div className='w-[15%]'>
+            <div
+              onClick={playCharacter}
+              className='relative flex w-full h-1/2 justify-center items-center hover-transition bg-black/50 hover:bg-green-500'>
+              <IconPlayerPlay className='text-white' />
+            </div>
+            <div
+              onClick={deleteCharacter}
+              className='relative flex w-full h-1/2 justify-center items-center hover-transition bg-black/50 hover:bg-red-500'>
+              <IconTrash className='text-white' />
+            </div>
+          </div>
+        </Transition>
       </div>
-      <Transition
-        show={characterIndex === props.index}
-        as={React.Fragment}
-        enter='scale opacity duration-300'
-        enterFrom='scale-[0.9] opacity-0'
-        enterTo='scale-1 opacity-100'
-        leave='scale duration-300'
-        leaveFrom='scale-1 opacity-100'
-        leaveTo='scale-[0.9] opacity-0'
-      >
-        <div className='w-[15%]'>
-          <div
-            onClick={playCharacter}
-            className='relative flex w-full h-1/2 justify-center items-center hover-transition bg-black/50 hover:bg-green-500'>
-            <IconPlayerPlay className='text-white' />
-          </div>
-          <div
-            onClick={deleteCharacter}
-            className='relative flex w-full h-1/2 justify-center items-center hover-transition bg-black/50 hover:bg-red-500'>
-            <IconTrash className='text-white' />
-          </div>
-        </div>
-      </Transition>
-    </div>
+    </>
   );
 };
 
