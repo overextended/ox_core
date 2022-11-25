2d2fa95b40c74c0a6b558f901e7114407e7e6b06
ESK0 <esko@hotmail.cz>
2022-11-25 21:43:48 +0100

    update (sql): column names escape

--- server/groups/db.lua
+++ server/groups/db.lua
@@ -1,13 +1,13 @@
 local MySQL = MySQL
 local db = {}
 
-local SELECT_GROUPS = 'SELECT * FROM ox_groups'
+local SELECT_GROUPS = 'SELECT * FROM `ox_groups`'
 ---Fetch all groups from the database.
 function db.selectGroups()
     return MySQL.query.await(SELECT_GROUPS)
 end
 
-local ADD_CHARACTER_TO_GROUP = 'INSERT INTO user_groups (charid, name, grade) VALUES (?, ?, ?)'
+local ADD_CHARACTER_TO_GROUP = 'INSERT INTO `user_groups` (`charid`, `name`, `grade`) VALUES (?, ?, ?)'
 ---Adds the group to the character.
 ---@param charid number
 ---@param name string
@@ -16,7 +16,7 @@ function db.addCharacterGroup(charid, name, grade)
     MySQL.prepare(ADD_CHARACTER_TO_GROUP, { charid, name, grade })
 end
 
-local UPDATE_CHARACTER_GROUP = 'UPDATE user_groups SET grade = ? WHERE charid = ? AND name = ?'
+local UPDATE_CHARACTER_GROUP = 'UPDATE `user_groups` SET `grade` = ? WHERE `charid` = ? AND `name` = ?'
 ---Update the character's grade for the given group.
 ---@param charid number
 ---@param name string
@@ -25,7 +25,7 @@ function db.updateCharacterGroup(charid, name, grade)
     MySQL.prepare(UPDATE_CHARACTER_GROUP, { grade, charid, name })
 end
 
-local REMOVE_CHARACTER_FROM_GROUP = 'DELETE FROM user_groups WHERE charid = ? AND name = ?'
+local REMOVE_CHARACTER_FROM_GROUP = 'DELETE FROM `user_groups` WHERE `charid` = ? AND `name` = ?'
 ---Removes the group from the user.
 ---@param charid number
 ---@param name string
--- server/player/db.lua
+++ server/player/db.lua
@@ -1,7 +1,7 @@
 local MySQL = MySQL
 local db = {}
 
-local SELECT_USERID = string.format('SELECT userid FROM users WHERE %s = ?', Server.PRIMARY_IDENTIFIER)
+local SELECT_USERID = string.format('SELECT `userid` FROM `users` WHERE %s = ?', Server.PRIMARY_IDENTIFIER)
 ---Select the userid for a player based on their identifier.
 ---@param identifier string
 ---@return number?
@@ -9,7 +9,7 @@ function db.getUserFromIdentifier(identifier)
     return MySQL.scalar.await(SELECT_USERID, { identifier })
 end
 
-local INSERT_USER = 'INSERT INTO users (username, license, steam, fivem, discord) VALUES (?, ?, ?, ?, ?)'
+local INSERT_USER = 'INSERT INTO `users` (`username`, `license`, `steam`, `fivem`, `discord`) VALUES (?, ?, ?, ?, ?)'
 ---Register a new user when a player first joins the server, and return their userid.
 ---@param username string
 ---@param identifiers {[string]: string}
@@ -19,7 +19,7 @@ function db.createUser(username, identifiers)
         { username, identifiers.license, identifiers.steam, identifiers.fivem, identifiers.discord }) --[[@as number?]]
 end
 
-local SELECT_CHARACTERS = 'SELECT charid, firstname, lastname, x, y, z, heading, DATE_FORMAT(last_played, "%d/%m/%Y") AS last_played FROM characters WHERE userid = ? AND deleted IS NULL'
+local SELECT_CHARACTERS = 'SELECT `charid`, `firstname`, `lastname`, `x`, `y`, `z`, `heading`, DATE_FORMAT(`last_played`, "%d/%m/%Y") AS `last_played` FROM `characters` WHERE `userid` = ? AND `deleted` IS NULL'
 ---Select all characters owned by the player.
 ---@param userid number
 ---@return table
@@ -27,7 +27,7 @@ function db.selectCharacters(userid)
     return MySQL.query.await(SELECT_CHARACTERS, { userid }) or {}
 end
 
-local SELECT_CHARACTER_DATA = 'SELECT is_dead AS isDead, gender, DATE_FORMAT(dateofbirth, "%d/%m/%Y") AS dateofbirth, phone_number as phoneNumber FROM characters WHERE charid = ?'
+local SELECT_CHARACTER_DATA = 'SELECT `is_dead` AS `isDead`, `gender`, DATE_FORMAT(`dateofbirth`, "%d/%m/%Y") AS `dateofbirth`, `phone_number` as `phoneNumber` FROM `characters` WHERE `charid` = ?'
 ---Select metadata for a character.
 ---@param charid any
 ---@return { isDead: boolean, gender: string, dateofbirth: string, phoneNumber: string }?
@@ -35,7 +35,7 @@ function db.selectCharacterData(charid)
     return MySQL.single.await(SELECT_CHARACTER_DATA, { charid })
 end
 
-local INSERT_CHARACTER = 'INSERT INTO characters (userid, firstname, lastname, gender, dateofbirth, phone_number) VALUES (?, ?, ?, ?, ?, ?)'
+local INSERT_CHARACTER = 'INSERT INTO `characters` (`userid`, `firstname`, `lastname`, `gender`, `dateofbirth`, `phone_number`) VALUES (?, ?, ?, ?, ?, ?)'
 ---Register a new character for the user and returns the charid.
 ---@param userid number
 ---@param firstName string
@@ -48,24 +48,21 @@ function db.createCharacter(userid, firstName, lastName, gender, date, phone_num
     return MySQL.prepare.await(INSERT_CHARACTER, { userid, firstName, lastName, gender, date, phone_number }) --[[@as number]]
 end
 
-local UPDATE_CHARACTER = [[UPDATE characters SET
-    x = ?, y = ?, z = ?, heading = ?, is_dead = ?, last_played = ?,
-    metadata = JSON_SET(metadata, "$.health", ?, "$.armour", ?)
-WHERE charid = ?]]
+local UPDATE_CHARACTER = [[UPDATE `characters` SET `x` = ?, `y` = ?, `z` = ?, `heading` = ?, `is_dead` = ?, `last_played` = ?, `metadata` = JSON_SET(metadata, "$.health", ?, "$.armour", ?) WHERE `charid` = ?]]
 ---Update character data for one or multiple characters.
 ---@param parameters table<number, any> | table<number, any>[]
 function db.updateCharacter(parameters)
     MySQL.prepare.await(UPDATE_CHARACTER, parameters)
 end
 
-local DELETE_CHARACTER = 'UPDATE characters SET deleted = curdate() WHERE charid = ?'
+local DELETE_CHARACTER = 'UPDATE `characters` SET `deleted` = curdate() WHERE `charid` = ?'
 ---Sets a character as deleted, preventing the user from accessing it.
 ---@param charid number
 function db.deleteCharacter(charid)
     return MySQL.update(DELETE_CHARACTER, { charid })
 end
 
-local SELECT_CHARACTER_GROUPS = 'SELECT name, grade FROM user_groups WHERE charid = ?'
+local SELECT_CHARACTER_GROUPS = 'SELECT `name`, `grade` FROM `user_groups` WHERE `charid` = ?'
 ---Select all groups the character is a member of.
 ---@param charid number
 ---@return { name: string, grade: number }[]?
@@ -73,8 +70,8 @@ function db.selectCharacterGroups(charid)
     return MySQL.query.await(SELECT_CHARACTER_GROUPS, { charid })
 end
 
-local UPDATE_METADATA = 'UPDATE characters SET metadata = JSON_SET(metadata, ?, ?) WHERE charid = ?'
-local REMOVE_METADATA = 'UPDATE characters SET metadata = JSON_REMOVE(metadata, ?) WHERE charid = ?'
+local UPDATE_METADATA = 'UPDATE `characters` SET `metadata` = JSON_SET(metadata, ?, ?) WHERE `charid` = ?'
+local REMOVE_METADATA = 'UPDATE `characters` SET `metadata` = JSON_REMOVE(metadata, ?) WHERE `charid` = ?'
 ---Update metadata for character.
 ---@param key string
 ---@param value any
@@ -87,7 +84,7 @@ function db.updateMetadata(key, value, charid)
     MySQL.prepare(REMOVE_METADATA, { key, charid })
 end
 
-local SELECT_METADATA = 'SELECT metadata FROM characters WHERE charid = ?'
+local SELECT_METADATA = 'SELECT `metadata` FROM `characters` WHERE `charid` = ?'
 ---Update metadata for character.
 ---@param charid number
 ---@return table<string, string | number | table>
