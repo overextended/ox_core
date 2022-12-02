90cf2c0b74149d5bb92cf6f6869d1e67397d0a80
ESK0 <esko@hotmail.cz>
2022-12-03 00:52:13 +0100

    inService leftover from different branch

--- server/player/events.lua
+++ server/player/events.lua
@@ -150,10 +150,10 @@ RegisterNetEvent('ox:setPlayerInService', function(job)
 
     if player and player.charid then
         if job and player.private.groups[job] then
-            return player:set('inService', job, true)
+            return player:set('inService', job)
         end
 
-        player:set('inService', false, true)
+        player:set('inService', false)
     end
 end)
 
