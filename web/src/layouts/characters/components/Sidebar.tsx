85affa712d18fffd6a859f00777304c199549b53
Luke <sabolukas03@gmail.com>
2022-12-07 15:22:26 +0100

    feat(web): spawn selector

--- web/src/layouts/characters/components/Sidebar.tsx
+++ web/src/layouts/characters/components/Sidebar.tsx
@@ -1,43 +1,57 @@
 import { useNuiEvent } from '../../../hooks/useNuiEvent';
 import { CharacterProps } from '../../../types';
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Transition } from '@headlessui/react';
 import CharacterSelector from '../index';
 import { debugData } from '../../../utils/debugData';
 import { useCharacterVisibilityState } from '../../../state/visibility';
 import { useSetCharacters, useSetMaxCharacterSlots } from '../../../state/characters';
+import { useSpawnsState } from '../../../state/spawn';
+import SpawnSelector from '../../spawn';
 
-debugData<{ characters: CharacterProps[]; maxSlots: number }>([
-  {
-    action: 'sendCharacters',
-    data: {
-      characters: [
-        {
-          firstname: 'Luke',
-          lastname: 'Lukensson',
-          last_played: '01/01/1999',
-          location: 'Somewhere far away',
-          gender: 'male',
-          dateofbirth: '01/01/1999',
-        },
-        {
-          firstname: 'Peter',
-          lastname: 'Petersson',
-          last_played: '01/01/1999',
-          location: 'Somewhere far away',
-          gender: 'male',
-          dateofbirth: '01/01/1999',
-        },
-      ],
-      maxSlots: 5,
-    },
-  },
-]);
+// debugData<{ characters: CharacterProps[]; maxSlots: number }>([
+//   {
+//     action: 'sendCharacters',
+//     data: {
+//       characters: [
+//         {
+//           firstname: 'Luke',
+//           lastname: 'Lukensson',
+//           last_played: '01/01/1999',
+//           location: 'Somewhere far away',
+//           gender: 'male',
+//           dateofbirth: '01/01/1999',
+//         },
+//         {
+//           firstname: 'Peter',
+//           lastname: 'Petersson',
+//           last_played: '01/01/1999',
+//           location: 'Somewhere far away',
+//           gender: 'male',
+//           dateofbirth: '01/01/1999',
+//         },
+//       ],
+//       maxSlots: 5,
+//     },
+//   },
+// ]);
+
+debugData<string[]>([{
+  action: 'sendSpawns',
+  data: [
+    'Last location',
+    'Legion Square',
+    'Route 86',
+    'Paleto Bay',
+    'Bollywood',
+  ],
+}]);
 
 export const Sidebar: React.FC = () => {
   const setCharacters = useSetCharacters();
   const [charactersVisible, setCharactersVisible] = useCharacterVisibilityState();
   const setCharacterMaxSlots = useSetMaxCharacterSlots();
+  const [spawns, setSpawns] = useSpawnsState();
 
   useNuiEvent('sendCharacters', (data: { characters: CharacterProps[]; maxSlots: number }) => {
     setCharacters(data.characters);
@@ -45,10 +59,14 @@ export const Sidebar: React.FC = () => {
     setCharactersVisible(true);
   });
 
+  useNuiEvent('sendSpawns', (data: string[]) => {
+    setSpawns(data);
+  });
+
   return (
     <Transition
       appear
-      show={charactersVisible}
+      show={charactersVisible || spawns.length > 0}
       as={Fragment}
       enter='ease-out duration-300'
       enterFrom='opacity-0 translate-x-[-100px]'
@@ -61,6 +79,9 @@ export const Sidebar: React.FC = () => {
         <div className='flex h-full flex-col justify-between font-text'>
           {charactersVisible && (
             <CharacterSelector />
+          )}
+          {spawns.length > 0 && (
+            <SpawnSelector />
           )}
         </div>
       </div>
