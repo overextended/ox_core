004fb7f4e599b024da99754aa8a3778a438ca966
Linden <65407488+thelindat@users.noreply.github.com>
2023-02-06 03:47:37 +1100

    refactor(client/events): new statebag handlers

--- client/events.lua
+++ client/events.lua
@@ -1,31 +1,36 @@
 local utils = require 'utils'
 
-AddStateBagChangeHandler('initVehicle', '', function(bagName, key, value, reserved, replicated)
-    if value then
-        local entity = GetEntityFromStateBagName(bagName)
+utils.entityStateHandler('initVehicle', function(entity)
+    -- workaround for server-vehicles that exist in traffic randomly creating peds
+    -- https://forum.cfx.re/t/sometimes-an-npc-spawns-inside-an-vehicle-spawned-with-createvehicleserversetter-or-create-automobile/4947251
+    for i = -1, 0 do
+        local ped = GetPedInVehicleSeat(entity, i)
 
-        if entity == 0 then
-            -- when would this even occur?
-            return print(('received invalid entity from statebag! (%s)'):format(bagName))
-        end
-
-        -- workaround for server-vehicles that exist in traffic randomly creating peds
-        -- https://forum.cfx.re/t/sometimes-an-npc-spawns-inside-an-vehicle-spawned-with-createvehicleserversetter-or-create-automobile/4947251
-        for i = -1, 0 do
-            local ped = GetPedInVehicleSeat(entity, i)
-
-            if ped ~= cache.ped and ped > 0 and NetworkGetEntityOwner(ped) == cache.playerId then
-                DeleteEntity(ped)
-            end
-        end
-
-        if NetworkGetEntityOwner(entity) == cache.playerId then
-            lib.setVehicleProperties(entity, value[1])
-            SetVehicleOnGroundProperly(entity)
-            SetVehicleDoorsLocked(entity, value[2])
-            Entity(entity).state:set('initVehicle', nil, true)
+        if ped ~= cache.ped and ped > 0 and NetworkGetEntityOwner(ped) == cache.playerId then
+            DeleteEntity(ped)
         end
     end
+
+    if not utils.awaitValue(function() return IsEntityWaitingForWorldCollision(entity) end) then return end
+
+    if NetworkGetEntityOwner(entity) ~= cache.playerId then return end
+
+    SetVehicleOnGroundProperly(entity)
+    Entity(entity).state:set('initVehicle', nil, true)
+end)
+
+utils.entityStateHandler('vehicleProperties', function(entity, _, value)
+    if not value or NetworkGetEntityOwner(entity) ~= cache.playerId then return end
+
+    lib.setVehicleProperties(entity, value)
+    Entity(entity).state:set('vehicleProperties', nil, true)
+end)
+
+utils.entityStateHandler('lockStatus', function(entity, _, value)
+    if not value or NetworkGetEntityOwner(entity) ~= cache.playerId then return end
+
+    SetVehicleDoorsLocked(entity, value)
+    Entity(entity).state:set('lockStatus', nil, true)
 end)
 
 lib.callback.register('ox:getNearbyVehicles', function(radius)
