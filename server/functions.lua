d9fec2dd7854730251c5f9ef973084af7386b77a
Linden <65407488+thelindat@users.noreply.github.com>
2023-02-10 21:12:56 +1100

    refactor(server/functions): remove SV_LAN identifier from GetIdentifiers

--- server/functions.lua
+++ server/functions.lua
@@ -9,18 +9,10 @@
 ---@param source number | string
 ---@return PlayerIdentifiers
 function Ox.GetIdentifiers(source)
-    ---@cast source string
-
-    if Shared.SV_LAN then
-        return {
-            [Server.PRIMARY_IDENTIFIER] = 'fayoum'
-        }
-    end
-
     local identifiers = {}
 
     for i = 0, GetNumPlayerIdentifiers(source) - 1 do
-        local prefix, identifier = string.strsplit(':', GetPlayerIdentifier(source, i))
+        local prefix, identifier = string.strsplit(':', GetPlayerIdentifier(source --[[@as string]], i))
 
         if prefix ~= 'ip' then
             identifiers[prefix] = identifier
