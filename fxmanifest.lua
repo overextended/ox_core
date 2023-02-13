84a132238e24d47b1ee5bd5444ac62d7cbf90f91
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2023-02-13 09:29:06 +0000

    chore: bump manifest version to v0.16.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ game         'gta5'
 
 --[[ Resource Information ]]--
 name         'ox_core'
-version      '0.15.1'
+version      '0.16.0'
 description  'What have I done?'
 license      'MIT'
 author       'overextended'
