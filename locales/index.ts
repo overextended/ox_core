192eb650ab33357e167b335c6930ea4b0efcc070
Linden <65407488+thelindat@users.noreply.github.com>
2024-01-13 23:33:25 +1100

    fix(locales): actually call it

--- locales/index.ts
+++ locales/index.ts
@@ -1,8 +1,6 @@
-// Is there a better day to do this?
-
 import { locale } from '@overextended/ox_lib/shared';
 
 type Locales = typeof import('../locales/en.json');
 
 //@ts-expect-error
-export default <K extends keyof Locales>(str: K, ...args: any[]): Locales[K] => locale;
+export default (str: keyof Locales, ...args: any[]): string => locale(str, ...args);
