0f425bb5110a5f4f079454609185e56a96ab943c
Linden <65407488+thelindat@users.noreply.github.com>
2024-01-16 01:34:46 +1100

    refactor(lib): typed exports

--- lib/client/index.ts
+++ lib/client/index.ts
@@ -1,7 +1,10 @@
 interface OxClient {
   [exportKey: string]: Function;
+  IsPlayerLoaded(): boolean;
+  GetPlayerData(): { userId: number; charId: number; stateId: string };
+  GetPlayerData(key: string): any;
 }
 
-export const Ox: OxClient = exports.ox_core;
+export const Ox: OxClient = exports.ox_core as any;
 
 export * from './player';
--- lib/server/index.ts
+++ lib/server/index.ts
@@ -1,7 +1,28 @@
+import type {
+  GetAccountById,
+  GetPlayerAccounts,
+  GetCharacterAccounts,
+  GetGroupAccounts,
+  AddAccountBalance,
+  RemoveAccountBalance,
+  TransferAccountBalance,
+} from '../../server/accounts';
+import type { OxPlayer } from '../../server/player/class';
+
 interface OxServer {
   [exportKey: string]: Function;
+  GetAccountById: typeof GetAccountById;
+  GetPlayerAccounts: typeof GetPlayerAccounts;
+  GetCharacterAccounts: typeof GetCharacterAccounts;
+  GetGroupAccounts: typeof GetGroupAccounts;
+  AddAccountBalance: typeof AddAccountBalance;
+  RemoveAccountBalance: typeof RemoveAccountBalance;
+  TransferAccountBalance: typeof TransferAccountBalance;
+  GetPlayer: typeof OxPlayer.get;
+  GetPlayers: typeof OxPlayer.getAll;
+  SaveAllPlayers: typeof OxPlayer.saveAll;
 }
 
-export const Ox: OxServer = exports.ox_core;
+export const Ox: OxServer = exports.ox_core as any;
 
 export * from './player';
