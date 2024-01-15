ca2b751f2f07b92dfef251df2550c798f50883fb
Linden <65407488+thelindat@users.noreply.github.com>
2024-01-16 03:57:59 +1100

    feat(sql): accounts table

--- sql/install.sql
+++ sql/install.sql
@@ -177,6 +177,19 @@ VALUES
   ('weapon', 'Weapon License'),
   ('driver', "Driver's License");
 
+CREATE TABLE
+  `accounts` (
+    `id` INT (6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `label` VARCHAR(50) NOT NULL,
+    `owner` INT UNSIGNED NULL,
+    `group` VARCHAR(20) NULL,
+    `balance` INT DEFAULT 0 NOT NULL,
+    `isDefault` TINYINT (1) DEFAULT 0 NOT NULL,
+    `type` ENUM ('personal', 'shared', 'group') DEFAULT 'personal' NOT NULL,
+    CONSTRAINT `FK_accounts_characters` FOREIGN KEY (`owner`) REFERENCES `characters` (`charId`) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT `FK_accounts_ox_groups` FOREIGN KEY (`group`) REFERENCES `ox_groups` (`name`) ON UPDATE CASCADE ON DELETE CASCADE
+  ) AUTO_INCREMENT = 100000;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
