915a0502755ac2008946b56db7ef8e3c39ee42df
Linden <65407488+thelindat@users.noreply.github.com>
2024-01-17 23:45:41 +1100

    feat(server/commands): /saveall command

---
+++ server/commands.ts
@@ -0,0 +1,15 @@
+import { addCommand } from '@overextended/ox_lib/server';
+import { OxPlayer } from 'player/class';
+import { OxVehicle } from 'vehicle/class';
+
+addCommand(
+  'saveall',
+  async () => {
+    OxPlayer.saveAll();
+    OxVehicle.saveAll();
+  },
+  {
+    help: 'Saves all players and vehicles to the database.',
+    restricted: 'group.admin',
+  }
+);
