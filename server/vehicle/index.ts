16e471ee780ecede62bd4124902fc83a2de45bfd
Linden <65407488+thelindat@users.noreply.github.com>
2024-04-24 19:22:55 +1000

    fix(server/vehicle): don't replace plate for existing vehicles

--- server/vehicle/index.ts
+++ server/vehicle/index.ts
@@ -81,7 +81,8 @@ export async function CreateVehicle(
   if (!data.vin && (data.owner || data.group)) data.vin = await OxVehicle.generateVin(vehicleData);
   if (data.vin && !data.owner && !data.group) delete data.vin;
 
-  data.plate = data.plate && (await IsPlateAvailable(data.plate)) ? data.plate : await OxVehicle.generatePlate();
+  data.plate =
+    data.plate && (!data.id || (await IsPlateAvailable(data.plate))) ? data.plate : await OxVehicle.generatePlate();
 
   const metadata = data.data || ({} as { properties: VehicleProperties; [key: string]: any });
   metadata.properties = metadata.properties || data.properties;
