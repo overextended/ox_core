2e0825f7886c7a1ba67fb7e88fd9ed75be41c431
Linden <65407488+thelindat@users.noreply.github.com>
2024-06-08 09:07:45 +1000

    feat(server/commands): add 'deletechar' command

--- server/player/commands.ts
+++ server/player/commands.ts
@@ -1,10 +1,27 @@
 import { addCommand } from '@overextended/ox_lib/server';
 import { OxPlayer } from 'player/class';
 
-addCommand('logout', async (playerId) => OxPlayer.get(playerId).logout(), {
+// NOTE: These commands are intended for development purposes.
+
+addCommand('logout', async (playerId) => OxPlayer.get(playerId).logout(true), {
   help: 'Logout and return to character selection.',
   restricted: 'group.admin',
 });
+
+addCommand(
+  'deletechar',
+  async (playerId) => {
+    const player = OxPlayer.get(playerId);
+
+    if (!player.charId) return;
+
+    player.deleteCharacter(player.charId);
+  },
+  {
+    help: 'Hard delete your current character (cannot be reversed).',
+    restricted: 'group.admin',
+  }
+);
 
 addCommand(
   'charinfo',
