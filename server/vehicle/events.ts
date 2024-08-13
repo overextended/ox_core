da3d5e497f07e3157e4f18d8591df3f104bb26ef
Linden <65407488+thelindat@users.noreply.github.com>
2024-08-13 12:27:41 +1000

    feat(server/vehicle): forcibly respawn vehicles deleted by clients

--- server/vehicle/events.ts
+++ server/vehicle/events.ts
@@ -2,10 +2,4 @@ import { OxVehicle } from './class';
 
 on('onResourceStop', (resource: string) => OxVehicle.saveAll(resource));
 
-on('entityRemoved', (entityId: number) => {
-  const vehicle = OxVehicle.get(entityId);
-
-  if (!vehicle) return;
-
-  vehicle.setStored('impound', true);
-});
+on('entityRemoved', (entityId: number) => OxVehicle.get(entityId)?.respawn());
