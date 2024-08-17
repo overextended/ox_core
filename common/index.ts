4f631336bcdf510b0fac55849591533019e06303
Robin Bird <googleoblivion@users.noreply.github.com>
2024-08-17 17:20:57 -0500

    fix(common/vehicle): Return vector3 from array for vehicle spawning.

--- common/index.ts
+++ common/index.ts
@@ -1,5 +1,6 @@
 import { checkDependency } from '@overextended/ox_lib/';
 import type { OxGroupPermissions } from 'types';
+import { Vector3 } from '@nativewrappers/fivem';
 
 if (!checkDependency('ox_lib', '3.24.0', true)) throw new Error(`Failed dependency check.`);
 
@@ -7,12 +8,14 @@ export function LoadDataFile(file: string) {
   return JSON.parse(LoadResourceFile('ox_core', `/common/data/${file}.json`));
 }
 
-export function VectorFromBuffer({ buffer }: any) {
+export function VectorFromBuffer({ buffer }: any): Vector3 {
   const arr = [];
 
-  for (let offset = 0; offset < buffer.length; offset += 4) arr.push(buffer.readFloatLE(offset));
+  for (let offset = 0; offset < buffer.length; offset += 4) {
+    arr.push(buffer.readFloatLE(offset));
+  }
 
-  return arr;
+  return new Vector3(arr[0], arr[1], arr[2]); // Use array elements for x, y, z
 }
 
 export function GetGroupPermissions(groupName: string): OxGroupPermissions {
