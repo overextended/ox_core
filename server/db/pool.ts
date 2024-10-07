89dd3e6188023a6f576d8943d581c6d926a367ca
Linden <65407488+thelindat@users.noreply.github.com>
2024-10-07 14:07:43 +1100

    feat(db/pool): add mariadb version validation
    Older versions may not support certain features, and may
    cause unexpected issues to occur.

--- server/db/pool.ts
+++ server/db/pool.ts
@@ -10,9 +10,15 @@ setImmediate(async () => {
   try {
     const dbPool = createPool(config);
     const version: string = (await dbPool.execute('SELECT VERSION() as version'))[0].version;
+    const recommendedDb = `Install MariaDB 11.4 for the best experience.\n- https://mariadb.com/kb/en/changes-improvements-in-mariadb-11-4/`;
 
     if (!version.toLowerCase().match('mariadb'))
-      return console.error(`ox_core is specifically designed for use with MariaDB. You are using MySQL ${version}.`);
+      return console.error(`MySQL ${version} is not supported. ${recommendedDb}`);
+
+    const [major, minor] = version.split('.');
+
+    if (+major < 11 || (+major === 11 && +minor < 4))
+      return console.error(`${version} is not supported. ${recommendedDb}`);
 
     console.log(`${`^5[${version}]`} ^2Database server connection established!^0`);
 
