1ee4540bac8529663f4e5eab6aa26029ba6c5d36
Linden <65407488+thelindat@users.noreply.github.com>
2024-10-07 13:38:55 +1100

    fix(db/pool): set pool after confirming a connection

--- server/db/pool.ts
+++ server/db/pool.ts
@@ -8,14 +8,15 @@ setImmediate(async () => {
   const config = GetConfig();
 
   try {
-    pool = createPool(config);
-
-    const version: string = (await pool.execute('SELECT VERSION() as version'))[0].version;
+    const dbPool = createPool(config);
+    const version: string = (await dbPool.execute('SELECT VERSION() as version'))[0].version;
 
     if (!version.toLowerCase().match('mariadb'))
       return console.error(`ox_core is specifically designed for use with MariaDB. You are using MySQL ${version}.`);
 
     console.log(`${`^5[${version}]`} ^2Database server connection established!^0`);
+
+    pool = dbPool;
   } catch (err) {
     console.log(
       `^3Unable to establish a connection to the database (${err.code})!\n^1Error ${err.errno}: ${err.message}^0`
