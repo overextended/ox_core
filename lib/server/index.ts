f1e9cbf5920ca871aa86e0aa0b0434d9916cfde2
Linden <65407488+thelindat@users.noreply.github.com>
2024-10-17 20:21:57 +1100

    fix(lib/server): removed exports

--- lib/server/index.ts
+++ lib/server/index.ts
@@ -2,14 +2,10 @@ import type { OxVehicle } from 'server/vehicle/class';
 import type { PayAccountInvoice, DeleteAccountInvoice } from 'server/accounts';
 import type { OxPlayer } from 'server/player/class';
 import type { GetCharIdFromStateId } from 'server/player/db';
-import type { DeleteAccount, DepositMoney, WithdrawMoney } from 'server/accounts/db';
 import type { DeleteGroup, GetGroupsByType, RemoveGroupPermission, SetGroupPermission } from 'server/groups';
 import { Ox as OxCore, OxCommon } from 'lib';
 
 interface OxServer extends OxCommon {
-  DeleteAccount: typeof DeleteAccount;
-  DepositMoney: typeof DepositMoney;
-  WithdrawMoney: typeof WithdrawMoney;
   SaveAllPlayers: typeof OxPlayer.saveAll;
   SaveAllVehicles: typeof OxVehicle.saveAll;
   GetCharIdFromStateId: typeof GetCharIdFromStateId;
