e3758fb8a4cfce63d0a6e64053e98d8e16ac7820
Linden <65407488+thelindat@users.noreply.github.com>
2024-10-26 11:51:31 +1100

    refactor(server): add version checking

--- server/index.ts
+++ server/index.ts
@@ -4,3 +4,9 @@ import 'player';
 import 'utils';
 import 'accounts';
 import 'vehicle';
+import { versionCheck } from '@overextended/ox_lib/server';
+import { DEBUG } from 'config';
+
+if (!DEBUG) {
+  versionCheck('overextended/ox_core');
+}
