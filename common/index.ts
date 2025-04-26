ef7b15cb692b61068a12235a55860c630ec090df
Zoo <34508112+FjamZoo@users.noreply.github.com>
2025-04-27 01:34:43 +0200

    feat(common/GetGroupPermissions): add export for group permissions

--- common/index.ts
+++ common/index.ts
@@ -16,3 +16,5 @@ console.info = (...args: any[]) => console.log(`^3${args.join('\t')}^0`);
 DEV: console.info(`Resource ${GetCurrentResourceName()} is running in development mode!`);
 
 import './vehicles';
+
+exports('GetGroupPermissions', GetGroupPermissions);
