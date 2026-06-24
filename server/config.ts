a47b88b75708f3d27e24086110cb958ffed503dd
Zoo <laurids112@live.dk>
2026-06-24 15:44:20 +0200

    feat(server/vehicle): only use entityRemoved event if not using sv_protectServerEntities

--- server/config.ts
+++ server/config.ts
@@ -4,4 +4,6 @@ export * from '../common/config';
 
 export const CREATE_DEFAULT_ACCOUNT = GetConvarInt('ox:createDefaultAccount', 1) === 1;
 
+export const PROTECT_SERVER_ENTITIES = GetConvarInt('sv_protectServerEntities', 0) === 1;
+
 if (DEBUG) SetConvar('ox:callbackTimeout', '1200000');
