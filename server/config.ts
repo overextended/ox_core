adbb7462e7785c13a515a4556fc744852a54da52
Zoo <34508112+FjamZoo@users.noreply.github.com>
2026-06-24 16:07:21 +0200

    refactor(server/vehicle): only use entityRemoved event if not using sv_protectServerEntities (#250)

--- server/config.ts
+++ server/config.ts
@@ -4,4 +4,6 @@ export * from '../common/config';
 
 export const CREATE_DEFAULT_ACCOUNT = GetConvarInt('ox:createDefaultAccount', 1) === 1;
 
+export const PROTECT_SERVER_ENTITIES = GetConvarBool('sv_protectServerEntities', false);
+
 if (DEBUG) SetConvar('ox:callbackTimeout', '1200000');
--- server/vehicle/events.ts
+++ server/vehicle/events.ts
@@ -1,5 +1,12 @@
 import { OxVehicle } from './class';
+import { PROTECT_SERVER_ENTITIES } from '../config';
 
 on('onResourceStop', (resource: string) => OxVehicle.saveAll(resource));
 
-on('entityRemoved', (entityId: number) => OxVehicle.getFromEntity(entityId)?.respawn());
+if (!PROTECT_SERVER_ENTITIES) {
+  console.warn(
+    "^3sv_protectServerEntities is not enabled - falling back to 'entityRemoved' event to respawn vehicles deleted by clients, which may degrade performance. Enable sv_protectServerEntities to avoid this.^0",
+  );
+
+  on('entityRemoved', (entityId: number) => OxVehicle.getFromEntity(entityId)?.respawn());
+}
